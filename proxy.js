ee43e98b4116634a0025a7019bb55fff8b216490
Saldous12 <iidklsdsadol775@gmail.com>
2024-11-17 11:57:20 -0600

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -2,21 +2,16 @@ const express = require('express');
 const httpProxy = require('http-proxy-middleware');
 const app = express();
 
-const targetUrl = 'https://www.google.com';  // The target site you're proxying
+const targetUrl = 'https://www.google.com'; // or other search engine
 
-// Proxy configuration to handle search requests
 app.use('/search', httpProxy.createProxyMiddleware({
   target: targetUrl,
   changeOrigin: true,
   pathRewrite: {
-    '^/search': '', // Remove /search from the path
+    '^/search': '',  // Remove /search from the URL
   },
 }));
 
-// Serve static assets from the public folder
-app.use(express.static('public'));
-
-// Start the server
 app.listen(3000, () => {
-  console.log('Proxy server running on http://localhost:3000');
+  console.log('Proxy server running at http://localhost:3000');
 });
