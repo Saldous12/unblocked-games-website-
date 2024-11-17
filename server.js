f8e66e81d9eb9b74c0e09767ace3d4ec5c8b2735
Saldous12 <iidklsdsadol775@gmail.com>
2024-11-17 11:24:11 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
+const express = require('express');
+const { createServer: createUltravioletServer } = require('./ultraviolet/index');
+const { createServer: createRammerheadServer } = require('./rammerhead/index');
 
+const app = express();
+
+// Serve static files
+app.use(express.static('public'));
+
+// Ultraviolet proxy
+app.use('/uv', createUltravioletServer());
+
+// Rammerhead proxy
+app.use('/rammerhead', createRammerheadServer());
+
+// Start the server
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
